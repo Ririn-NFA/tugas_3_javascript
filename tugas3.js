80f5ae2e6100e08e9503b9b67fe44fe2c0583b3a
Ririn <ririn.nfa@gmail.com>
2020-11-03 02:43:26 +0700

    Tugas 3 Javascript
    Function with Parameter

---
+++ tugas3.js
@@ -0,0 +1,16 @@
+function valueInput(value) {
+    console.log("ini nilai value sebelum diubah " + value);
+
+    value = 10;
+    return value;
+}
+
+var newValue = valueInput(5);
+console.log("ini nilai value setelah diubah " + newValue);
+
+
+
+
+
+
+
